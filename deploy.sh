1c1e9321fb92b5394f4ebccd1cd0b773dfa4a1b6
patricksu <pengsu9@gmail.com>
2020-02-06 23:35:50 -0800

    let us deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+
+docker build -t mightycontainer002/multi-client:latest -t mightycontainer002/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mightycontainer002/multi-server:latest -t mightycontainer002/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mightycontainer002/multi-worker:latest -t mightycontainer002/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push mightycontainer002/multi-client:latest
+docker push mightycontainer002/multi-client:$SHA
+docker push mightycontainer002/multi-server:latest
+docker push mightycontainer002/multi-server:$SHA
+docker push mightycontainer002/multi-worker:latest
+docker push mightycontainer002/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=mightcontainer002/multi-server:$SHA
+kubectl set image deployments/client-deployment client=mightcontainer002/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=mightcontainer002/multi-worker:$SHA
